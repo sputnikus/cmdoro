52ae3ce48e7cfcdc27ff68f49ee0d0631bad3e99
Martin Putniorz <mputniorz@gmail.com>
2012-08-07 11:17:43 +0200

    Some of the necessary code

---
+++ cmdoro/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    cmdoro
+    ~~~~~~
+
+    Pomodoro timer for your shell (and also funny little side project)
+
+    :copyright: (c) 2012 by Martin Putniorz.
+    :license: BSD, see LICENSE for details.
+"""
