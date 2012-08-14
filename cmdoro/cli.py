26949bd2be871beace88aae1702b25dec1b289b5
Martin Putniorz <mputniorz@gmail.com>
2012-08-14 18:18:42 +0200

    Simple timer, start curses

---
+++ cmdoro/cli.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    cmdoro.cli
+    ~~~~~~~~~~
+
+    Countdown CLI in curses (with some colors and stuff)
+
+    :copyright: (c) 2012 by Martin Putniorz.
+    :license: BSD, see LICENSE for details.
+"""
+import curses
---
+++ cmdoro/timer.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+    cmdoro.timer
+    ~~~~~~~~~~~~
+
+    Timer functions (for final countdown)
+
+    :copyright: (c) 2012 by Martin Putniorz.
+    :license: BSD, see LICENSE for details.
+"""
+import time
+
+
+def countdown(seconds):
+    """The countdown generator, yields on minute change
+
+    :param seconds: Countdown time in seconds
+    """
+    trail = seconds % 60
+
+    if trail:
+        # trailing seconds, we want to count by a minute
+        time.sleep(trail)
+        seconds -= trail
+        yield seconds / 60
+
+    while seconds > 0:
+        # now yield each minute
+        time.sleep(60)
+        seconds -= 60
+        yield seconds / 60
