d53deccc9299a9b3a4f92c71126a108bd96ff757
Martin Putniorz <mputniorz@gmail.com>
2012-08-26 16:40:39 +0200

    Context change

--- cmdoro/cli.py
+++ cmdoro/cli.py
@@ -9,40 +9,68 @@
     :license: BSD, see LICENSE for details.
 """
 import sys
-import time
 
 import urwid
 
 from config import loader
-from signals import MinuteSig, DoneSig
 
 
-def countdown(minutes):
-    """The countdown generator, yields on minute change
+class CmdoroWidget(urwid.Text):
+    """CMDoro timer widget"""
 
-    :param minutes: Countdown time in minutes
-    """
+    _selectable = True
+    signals = ['started', 'switch']
 
-    for minute in range(minutes, 0, -1):
-        yield minutes
-        time.sleep(60)
-
-
-def main():
-    """Main screen of cmdoro"""
-    try:
+    def __init__(self, *args, **kwargs):
         config_dict = loader()
-    except IOError:
-        text = urwid.Text('CMDoro: Invalid config file!')
-        filler = urwid.Filler(text, 'top')
-        loop = urwid.MainLoop(filler, unhandled_input=unhandled_input)
-        loop.run()
-        sys.exit(1)
+        self.work_time = config_dict['work_time']
+        self.rest_time = config_dict['rest_time']
 
-    text = urwid.Text('CMDoro version 0.1')
-    filler = urwid.Filler(text, 'top')
-    loop = urwid.MainLoop(filler, unhandled_input=unhandled_input)
-    loop.run()
+        self.started = False
+        self.time = self.work_time
+        self._context = 'work'
+        super(CmdoroWidget, self).__init__('CMDoro version 0.1. Press spacebar \
+and get shit done.')
+
+    def update(self):
+        if self.started:
+            if self._context == 'work':
+                self.set_text('Working now. Remaining time: %d minutes'\
+                    % self.time)
+            else:
+                self.set_text('Resting now. Remaining time: %d minutes'\
+                    % self.time)
+        self.time -= 1
+        if self.time:
+            return True
+        else:
+            self._emit('switch')
+
+    def start(self):
+        self.started = True
+        self._emit('started')
+
+    def keypress(self, size, key):
+        if key == ' ':
+            self.start()
+        else:
+            return key
+        if not self.started:
+            self.set_text('Neco')
+
+    def get_context(self):
+        return self._context
+
+    def set_context(self, new_context):
+        if new_context in ('work', 'rest'):
+            self._context = new_context
+            if new_context == 'work':
+                self.time = self.work_time
+            else:
+                self.time = self.rest_time
+        self._emit('started')
+
+    context = property(get_context, set_context)
 
 
 def unhandled_input(key_input):
@@ -53,11 +81,35 @@ def unhandled_input(key_input):
         raise urwid.ExitMainLoop()
 
 
-def working():
-    """Working stage countdown"""
-    pass
+def update_timer(loop, cmdoro):
+    if not cmdoro.started:
+        return
+    if cmdoro.update():
+        loop.set_alarm_in(60, update_timer, cmdoro)
 
 
-def resting():
-    """Resting stage countdown"""
-    pass
+def switch_update_timer(cmdoro, loop):
+    update_timer(loop, cmdoro)
+
+
+def switch_context(cmdoro):
+    cmdoro.context = 'rest' if cmdoro.context == 'work' else 'work'
+
+
+def main():
+    """Main urwid loop"""
+    try:
+        cmdoro = CmdoroWidget()
+    except IOError:
+        text = urwid.Text('CMDoro: Invalid config file!')
+        filler = urwid.Filler(text, 'top')
+        loop = urwid.MainLoop(filler, unhandled_input=unhandled_input)
+        loop.run()
+        sys.exit(1)
+
+    cmdoro_fill = urwid.Filler(cmdoro, 'top')
+
+    loop = urwid.MainLoop(cmdoro_fill, unhandled_input=unhandled_input)
+    urwid.connect_signal(cmdoro, 'started', switch_update_timer, loop)
+    urwid.connect_signal(cmdoro, 'switch', switch_context)
+    loop.run()
